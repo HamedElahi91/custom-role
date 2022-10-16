152f01dfb2a1a10321258980a95474ba2fa7bb44
Hamed <elahii.hamed@gmail.com>
2022-10-16 11:25:47 +0330

    First commit for basic files and folders

---
+++ includes/class-custom-role-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://egeekbin.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Role
+ * @subpackage Custom_Role/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Role
+ * @subpackage Custom_Role/includes
+ * @author     Hamed Elahi <elahi.hamed@hotmail.com>
+ */
+class Custom_Role_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-role-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://egeekbin.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Role
+ * @subpackage Custom_Role/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Role
+ * @subpackage Custom_Role/includes
+ * @author     Hamed Elahi <elahi.hamed@hotmail.com>
+ */
+class Custom_Role_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
