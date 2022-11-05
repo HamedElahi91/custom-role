ad14a0517e79c7ebc8a82c962707a7e5c07f7919
Hamed <elahii.hamed@gmail.com>
2022-11-05 13:07:33 +0330

    no msg

--- admin/partials/custom_role_all_roles.php
+++ admin/partials/custom_role_all_roles.php
@@ -108,7 +108,95 @@ function custom_role_show_fixed_customer_func(){
 <?php
 }
 function custom_role_show_requests_func(){
-  echo '<div style = "font-size: 200%;margin: 10px;text-align: center;background-color: white;padding: 10px;border-radius: 28px;">Requests</div>';
+  ?>
+    <div style = "font-size: 200%;margin: 10px;text-align: center;background-color: white;padding: 10px;border-radius: 28px;"> 
+      <?php echo esc_html( get_admin_page_title() ); ?>
+    </div>;
+  <?php 
+  $default_tab = 'major-buyer-requests';
+  $tab = isset($_GET['tab']) ? $_GET['tab'] : $default_tab;
+  global $wpdb;
+  $custom_role_request_table_name = $wpdb->prefix . 'customer_requests_custom_role_table';
+  $major_buyer_request_results = $wpdb->get_results("SELECT * FROM $custom_role_request_table_name WHERE request_type = 'major_buyer' ");
+  $fixed_customer_request_results = $wpdb->get_results("SELECT * FROM $custom_role_request_table_name WHERE request_type = 'fixed_customer' ");
+  ?>
+    <div class="wrap">    
+      <!-- Here are our tabs -->
+      <nav class="nav-tab-wrapper">
+        <a href="?page=requests.php&tab=major-buyer-requests" class="nav-tab <?php if($tab=='major-buyer-requests'):?>nav-tab-active<?php endif; ?>">Major Buyers requests</a>
+        <a href="?page=requests.php&tab=fixed-customer-requests" class="nav-tab <?php if($tab=='fixed-customer-requests'):?>nav-tab-active<?php endif; ?>">Fixed Customer requests</a>
+        
+      </nav>
+
+      <div class="tab-content">
+      <?php switch($tab) :
+        case 'major-buyer-requests':
+          
+          echo 'major_buyer'; //Put your HTML here
+          if(!empty($major_buyer_request_results)){
+            ?>
+              <table class="widefat" style="border-radius: 15px;">
+                <thead>
+                <tr>
+                  <th>id</th>
+                  <th>Name</th>
+                  <th>customerid</th>
+                  <th>action</th>
+                </tr>
+                </thead>
+                <?php foreach($major_buyer_request_results as $row): ?>
+                <tr>
+                  <td><?php echo $row->id ; ?></td>
+                  <td><?php echo $row->username ; ?></td>
+                  <td><?php echo $row->customerid ; ?></td>
+                  <td>
+                      <a href="#" class= "confirm-requests" id="conf_major_buyer" >confirm</a>
+                      ||
+                      <a href="#" class= "confirm-requests" id="refuse_major_buyer">refuse</a>
+                  </td>
+                </tr>
+                <?php endforeach ?>    
+              </table>
+            <?php 
+        }
+          
+          break;
+        case 'fixed-customer-requests':
+          echo 'fixed_customer_requests';
+          if(!empty($fixed_customer_request_results)){
+            ?>
+              <table class="widefat" style="border-radius: 15px;">
+                <thead>
+                <tr>
+                  <th>id</th>
+                  <th>Name</th>
+                  <th>customerid</th>
+                  <th>action</th>
+                </tr>
+                </thead>
+                <?php foreach($fixed_customer_request_results as $row): ?>
+                <tr>
+                  <td><?php echo $row->id ; ?></td>
+                  <td><?php echo $row->username ; ?></td>
+                  <td><?php echo $row->customerid ; ?></td>
+                  <td>
+                      <a href="#" class= "confirm-requests" id="conf_fixed_customer">confirm</a>
+                      ||
+                      <a href="#" class= "confirm-requests" id="refuse_fixed_customer">refuse</a>
+                  </td>
+                </tr>
+                <?php endforeach ?>    
+              </table>
+            <?php 
+        }
+          break;
+        default:
+          echo 'Default tab';
+          break;
+      endswitch; ?>
+      </div>
+    </div>
+  <?php
 }
 
 function custom_role_show_settings_func(){
